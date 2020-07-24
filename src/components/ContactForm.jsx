ef2de06093e1ad007e72b0b461a0cafffb35d6e4
Natalie McCroy <nataliemccroy@gmail.com>
2020-07-23 21:19:05 -0500

    more form styling

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -5,12 +5,13 @@ import Button from "components/_ui/Button";
 
 
 const ContactContainer = styled("div")`
-    max-height: 900px;
+    max-height: 1000px;
     overflow: hidden;
     display: flex;
     flex-direction: column;
-    justify-content: flex-start;
-    align-items: stretch;
+    // justify-content: space-around;
+    // align-items: flex-start;
+    // align-content: flex-end;
     margin-bottom: 1.23em;
 
     img {
@@ -22,10 +23,10 @@ const Input = styled.input`
   outline: 0;
   padding: 1rem 1rem;
   margin-bottom: 1rem;
-  margin: 0 auto;
+  margin: 0px auto;
   border: 2px solid rgba(34, 36, 38, 0.15);
   border-radius: 3px;
-  min-width: 80%;
+  min-width: 90%;
   display: flex;
   flex-direction: column;
   &:focus,
@@ -38,22 +39,33 @@ const ContactHeader = styled.h2`
   color: pink;
 `;
 
+const ModButton = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
 
-// const Label = styled.input`
-//   color: pink;
-// `;
+
+const Label = styled.div`
+  color: pink;
+  height: 90px;
+  display: flex;
+  align-items: center;
+`;
 
 const TextArea = styled.input`
     outline: 0;
-    padding: 1rem 1rem;
+    padding: 3rem 1rem;
     border: 2px solid rgba(34, 36, 38, 0.15);
     border-radius: 3px;
-    margin: 0 auto;
+    margin: 8rem auto;
     margin-bottom: 50px;
-    min-width: 80%;
-    height: 90px;
+    min-width: 90%;
+    height: 20px;
     display: flex;
     flex-direction: column;
+    justify-content: flex-start;
     &:focus,
     &:active {
     border-color: #85b7d9;
@@ -69,23 +81,25 @@ const ContactForm = () => (
         <form method="post" netlify-honeypot="bot-field" data-netlify="true" name="contact">
             <input type="hidden" name="bot-field" />
             <input type="hidden" name="form-name" value="contact" />
-        <label for="name">
-            Name
+        <Label for="name">
+            Name:
             <Input type="text" name="name" id="name" />
-        </label>
-        <label for="email">
-            Email
+        </Label>
+        <Label for="email">
+            Email:
             <Input type="email" name="email" id="email" />
-        </label>
-        <label for="subject">
-            Subject
+        </Label>
+        <Label for="subject">
+            Subject:
             <Input type="text" name="subject" id="subject" />
-        </label>
-        <label for="message">
-            Message
+        </Label>
+        <Label for="message">
+            Message:
             <TextArea name="message" id="message" rows="5"/>
-        </label>
+        </Label>
+        <ModButton>
         <Button type="submit" className="Button--secondary">Send</Button>
+        </ModButton>
         </form>
     </ContactContainer>
     </>
